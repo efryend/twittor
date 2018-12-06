5d0f29c75cf8a8262f441939c3cf9466955b04fe
efryend <my.efryend@gmail.com>
2018-12-06 17:05:46 -0600

    Modificamos la ruta

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 
+var url = window.location.href;
+var swLocation = '/twittor/sw.js'
+
+
 if( navigator.serviceWorker ){
+
+    if( url.includes('localhost') ){
+        swLocation = '/sw.js';
+    }
+
     navigator.serviceWorker.register('/sw.js');
 }
 
